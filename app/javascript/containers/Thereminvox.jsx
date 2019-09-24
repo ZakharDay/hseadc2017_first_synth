c6dd71b380f95d1923dffe709c8ea54ccc69fd26
ZakharDay <zakhar.day@gmail.com>
2019-09-24 15:06:38 +0300

    Work snapshot

--- app/javascript/containers/Thereminvox.jsx
+++ app/javascript/containers/Thereminvox.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tone from 'tone'
 
 export default class Thereminvox extends React.Component {
   constructor(props) {
@@ -30,6 +31,7 @@ export default class Thereminvox extends React.Component {
     this.changeFrequency = this.changeFrequency.bind(this)
     this.changeDetune = this.changeDetune.bind(this)
     this.changeVisualization = this.changeVisualization.bind(this)
+    this.handleSynthPlay = this.handleSynthPlay.bind(this)
   }
 
   componentDidMount() {
@@ -55,6 +57,89 @@ export default class Thereminvox extends React.Component {
     } else {
       this.handleStart()
     }
+  }
+
+  handleSynthPlay() {
+    console.log('Yo!')
+    let synth = new Tone.Synth().toMaster()
+    // synth.triggerAttackRelease('A4', '4n')
+
+    // let pattern = new Tone.Pattern(
+    //   function(time, note) {
+    //     synth.triggerAttackRelease(note, '16n')
+    //   },
+    //   ['C4', 'D4', 'E4', 'G4', 'A4']
+    // )
+    //
+    // pattern.start(0)
+
+    // let loop = new Tone.Loop(function(time) {
+    //   synth.triggerAttackRelease('C2', '8n', time)
+    // }, '4n')
+    //
+    // loop.start('1m').stop('4m')
+
+    // let synth = new Tone.Synth({
+    //   oscillator: {
+    //     type: 'pwm',
+    //     modulationFrequency: 0.2
+    //   },
+    //   envelope: {
+    //     attack: 0.02,
+    //     decay: 0.1,
+    //     sustain: 0.2,
+    //     release: 0.1
+    //   }
+    // }).toMaster()
+    //
+    // synth.triggerAttack('D3', '1')
+
+    // let polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster()
+    // polySynth.triggerAttackRelease(['C4', 'E4', 'G4', 'B4'], '2n')
+    //
+    // let distortion = new Tone.Distortion(0.4).toMaster()
+    // polySynth.connect(distortion)
+
+    // let pwm = new Tone.PWMOscillator('Bb3').toMaster().start()
+
+    let part = new Tone.Part(
+      function(time, pitch) {
+        synth.triggerAttackRelease(pitch, '8n', time)
+      },
+      [['0', 'C#3'], ['4n', 'G3'], [3 * Tone.Time('8n'), 'G#3'], ['2n', 'C3']]
+    )
+
+    // part.start('4m')
+
+    let loop = new Tone.Loop(function(time) {
+      // synth.triggerAttackRelease('C2', '8n', time)
+      part.start('1n')
+    }, '4n')
+
+    loop.start('1n') //.stop('4m')
+
+    Tone.Transport.bpm.value = 220
+    Tone.Transport.start()
+
+    // let note = new Tone.Event(function(time, pitch) {
+    //   synth.triggerAttackRelease(pitch, '16n', time)
+    // }, 'C2')
+    //
+    // note.set({
+    //   loop: true,
+    //   loopEnd: '2n'
+    // })
+    //
+    // note.start(0)
+    // note.stop('4m')
+
+    // Tone.Transport.scheduleRepeat(
+    //   function(time) {
+    //     synth.triggerAttack(time)
+    //   },
+    //   '8n',
+    //   '1m'
+    // )
   }
 
   handleStart() {
@@ -121,8 +206,6 @@ export default class Thereminvox extends React.Component {
 
     let elements = []
 
-    console.log(this.state)
-
     if (fftData != undefined) {
       fftData.map(function(fftParam, i) {
         elements.push(
@@ -137,6 +220,7 @@ export default class Thereminvox extends React.Component {
 
     return (
       <div>
+        <div onClick={this.handleSynthPlay}>Synth</div>
         <div onClick={this.handleStartOrStopClick}>{button}</div>
         <div className="analyser">{elements}</div>
       </div>
